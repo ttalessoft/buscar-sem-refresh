e3ef963cc38c48649b3a915a7550521e323e22d9
ttalessoft <ttalessoft@gmail.com>
2018-12-18 18:46:56 -0200

    trabalhando com busca sem refresh

---
+++ conecta.php
@@ -0,0 +1,8 @@
+<?php
+
+    $servidor = "localhost";
+    $usuario = "root";
+    $senha = "";
+    $db = "db_banco_teste";
+
+    $mysqli = new mysqli($servidor, $usuario, $senha, $db);
---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Busca sem refresh</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <!-- css -->
+    <link rel="stylesheet" type="text/css" media="screen" href="stylesheet.css" />
+
+    <!-- javascript -->
+    <script src="jquery-3.3.1.js"></script>    
+    <script src="javascript.js"></script>
+
+</head>
+<body>
+    <h1>Busca sem refresh</h1>
+
+    <form action="processa.php" methot="post">
+
+        Buscar por: <input type="text" name="campo" id="campo">
+
+        <div id="resultado">
+
+            <?php 
+
+                include_once("conecta.php");
+                $sql = $mysqli->prepare("select * from tb_produtos");
+                $sql->execute();
+                $sql->bind_result($id, $descricao, $vlr, $data);
+
+                echo "
+                    <table>
+                        <thead>
+                            <tr>
+                                <td>#</td>
+                                <td>Descrição</td>
+                                <td>Valor</td>
+                                <td>Dt. Cadastro</td>
+                            </tr>
+                        </thead>
+                        <tbody";
+
+                    while ($sql->fetch()) {
+                        echo "
+                        <tr>
+                            <td>$id</td>
+                            <td>$descricao</td>
+                            <td>$vlr</td>
+                            <td>$data</td>
+                        </tr>";
+                    }
+                    echo "</tbody>
+                    </table>";
+
+            ?>
+
+        </div>
+    
+    </form>
+</body>
+</html>
